cd4e62d1874e4ca3009c057bc0cc1c14a51fbce4
Konrad Krawczyk <kk3144@nyu.edu>
2021-06-15 00:40:46 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactHlsPlayer from 'react-hls-player';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactHlsPlayer
+        src="https://cdn.livepeer.com/hls/9600iz2b6y8wrag7/index.m3u8"
+        autoPlay={false}
+        controls={true}
+        width="100%"
+        height="auto"
+      />
     </div>
   );
 }
